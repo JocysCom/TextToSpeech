45dffd80021bbf7f917f9a6a79d35257a0404e79
Vaidotas Jocys <vaidotas@jocys.com>
2015-03-01 18:46:40 +0000

    Jocys.com Text to Speech WoW Addon update to version 2.2.13 (2015-03-01)

--- TextToSpeech/WoW/JocysCom-TextToSpeech-WoW/JocysCom-TextToSpeech-WoW.lua
+++ TextToSpeech/WoW/JocysCom-TextToSpeech-WoW/JocysCom-TextToSpeech-WoW.lua
@@ -11,7 +11,7 @@ local messageDoNotDisturb = "Please wait... NPC dialog window is open and text-t
 -- Set text.
 local function JocysCom_Text_EN()
 	-- Titles.
-	JocysCom_OptionsFrame.TitleText:SetText("Jocys.com Text to Speech World of Warcraft Addon 2.2.12 ( 2015-02-14 )");
+	JocysCom_OptionsFrame.TitleText:SetText("Jocys.com Text to Speech World of Warcraft Addon 2.2.13 ( 2015-03-01 )");
 	-- Frames.
 	JocysCom_ScrollFrame.text:SetText("When mouse pointer is over this frame...\n\nSCROLL UP will START SPEECH\n\nSCROLL DOWN will STOP SPEECH");
 	-- Check buttons.
