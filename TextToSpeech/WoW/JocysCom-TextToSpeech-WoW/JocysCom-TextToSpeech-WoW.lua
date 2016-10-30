48d8b2daacc3e6ec738a8dcf03d766206967adcb
Vaidotas Jocys <vaidotas@jocys.com>
2016-10-30 04:08:10 +0000

    Addon update to 2.2.75 (2016-10-30)

--- TextToSpeech/WoW/JocysCom-TextToSpeech-WoW/JocysCom-TextToSpeech-WoW.lua
+++ TextToSpeech/WoW/JocysCom-TextToSpeech-WoW/JocysCom-TextToSpeech-WoW.lua
@@ -64,7 +64,7 @@ end
 -- Set text.
 function JocysCom_Text_EN()
 	-- OptionsFrame title.
-	JocysCom_OptionsFrame.TitleText:SetText("Jocys.com Text to Speech World of Warcraft Addon 2.2.72 ( 2016-10-29 )");
+	JocysCom_OptionsFrame.TitleText:SetText("Jocys.com Text to Speech World of Warcraft Addon 2.2.75 ( 2016-10-30 )");
 	-- CheckButtons (Options) text.
 	JocysCom_FilterCheckButton.text:SetText("|cff808080 Hide addon|r |cffffffff<messages>|r |cff808080in chat window.|r");
     JocysCom_SaveCheckButton.text:SetText("|cff808080 Hide addon messages|r |cffffffffNo NPC target|r|cff808080 or |r|cffffffffTarget is not NPC|r |cff808080and|r |cffffffff<target> is already in the list|r |cff808080in chat window.|r");
@@ -487,8 +487,8 @@ function JocysCom_SpeakMessage(speakMessage, event, name, group)
 		local chatMessageE = "</part></message>";
 		local chatMessage;
 		local chatMessageLimit = 240;
-		local sizeAdd = chatMessageLimit - string.len(chatMessageSA) - string.len(chatMessageE);
-		local sizePlay = chatMessageLimit - string.len(chatMessageSP) - string.len(chatMessageE);
+		local sizeAdd = chatMessageLimit - string.len(chatMessageSA) - string.len(chatMessageE) - string.len(addonPrefix);
+		local sizePlay = chatMessageLimit - string.len(chatMessageSP) - string.len(chatMessageE) - string.len(addonPrefix);
 		if DebugEnabled then print("Add message size: " ..  sizeAdd .. " Play message size: " .. sizePlay) end	
 		local startIndex = 1;
 		local endIndex = 1;
