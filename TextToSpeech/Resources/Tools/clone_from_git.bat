7b4da397efc2d5bc9dc7e26aa27a5bfad7960d1e
Evaldas Jocys <evaldas@jocys.com>
2017-03-22 11:56:26 +0000

    Remove duplicated file

--- TextToSpeech/Resources/Tools/clone_from_git.bat
+++
@@ -1,5 +0,0 @@
-SET prg="%ProgramFiles%\Git\bin\git.exe"
-IF NOT EXIST %prg% SET prg="%ProgramFiles(x86)%\Git\bin\git.exe"
-IF NOT EXIST %prg% SET prg="%ProgramW6432%\Git\bin\git.exe"
-%prg% clone https://github.com/JocysCom/TextToSpeech.git "D:\Projects\Jocys.com\TextToSpeech.Temp"
-pause
