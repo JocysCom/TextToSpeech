e75b403f6d73ee19381182b809e360b1ba02f1b6
Vaidotas Jocys <vaidotas@jocys.com>
2017-04-03 17:29:20 +0100

    Addon version updated for WOW 7.2.0.

--- TextToSpeech/WoW/JocysCom-TextToSpeech-WoW/JocysCom-TextToSpeech-WoW.lua
+++ TextToSpeech/WoW/JocysCom-TextToSpeech-WoW/JocysCom-TextToSpeech-WoW.lua
@@ -130,7 +130,7 @@ end
 -- Set text.
 function JocysCom_Text_EN()
 	-- OptionsFrame title.
-	JocysCom_OptionsFrame.TitleText:SetText("Jocys.com Text to Speech World of Warcraft Addon 2.2.88 ( 2016-12-18 )");
+	JocysCom_OptionsFrame.TitleText:SetText("Jocys.com Text to Speech World of Warcraft Addon 2.2.90 ( 2017-04-03 )");
 	-- CheckButtons (Options) text.
 	JocysCom_FilterCheckButton.text:SetText("|cff808080 Hide addon|r |cffffffff<messages>|r |cff808080in chat window.|r");
 	JocysCom_SaveCheckButton.text:SetText("|cff808080 Hide addon|r |cffffffffSave in Monitor <NPC>|r |cff808080 " .. macroName .. " related messages.|r");
